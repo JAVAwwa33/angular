434defc0f77c104ffa3bfcfe2332d8ccf60563eb
lkre <lukasz.res@gmail.com>
2021-06-12 14:05:45 +0200

    Zad 4

--- src/app/my-component/my-component.component.ts
+++ src/app/my-component/my-component.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-my-component',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-component.component.css']
 })
 export class MyComponentComponent implements OnInit {
+
+  @Input()
+  movie: any;
 
   constructor() { }
 
