ae91954d474feac8c80d15d98b54baff097ea3ca
lkre <lukasz.res@gmail.com>
2021-06-12 14:37:05 +0200

    Zad 5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { MyComponentComponent } from './my-component/my-component.component';
+import { TabConentComponent } from './tab-conent/tab-conent.component';
+import { TabComponentComponent } from './tab-component/tab-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyComponentComponent
+    MyComponentComponent,
+    TabConentComponent,
+    TabComponentComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/tab-conent/tab-conent.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tab-conent',
+  templateUrl: './tab-conent.component.html',
+  styleUrls: ['./tab-conent.component.css']
+})
+export class TabConentComponent implements OnInit {
+
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
