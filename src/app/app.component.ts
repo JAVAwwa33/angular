ff244d7ca944b5e4af190263d789454f46dbe86b
lkre <lukasz.res@gmail.com>
2021-06-12 12:55:09 +0200

    Zad 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-project';
+  exists: boolean = true;
+
+  toggleExists(): void{
+    this.exists = !this.exists;
+  }
 }
