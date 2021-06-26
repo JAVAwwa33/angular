c38469355c2b412486010ca5302429b4cce76448
lkre <lukasz.res@gmail.com>
2021-06-26 15:46:10 +0200

    Zad 21

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,4 @@
 import {Component, OnInit} from '@angular/core';
-import {HttpClient, HttpHeaders} from "@angular/common/http";
-import {FormBuilder} from "@angular/forms";
-import {Post} from "./models/Post";
 
 @Component({
   selector: 'app-root',
@@ -10,30 +7,9 @@ import {Post} from "./models/Post";
 })
 export class AppComponent implements OnInit {
 
-  signupForm: any;
-
-  constructor(private http: HttpClient, formBuilder: FormBuilder) {
-    this.signupForm = formBuilder.group({
-      id: [''],
-      title: [''],
-      userId: ['']
-    });
+  constructor() {
   }
 
   ngOnInit(): void {
-    this.loadValues();
-  }
-
-
-  loadValues() : void {
-    this.http.get('https://jsonplaceholder.typicode.com/posts/1'
-    )
-      .subscribe(
-        (data) => {
-          const post = data as Post;
-          this.signupForm.controls.id.patchValue(post.id);
-          this.signupForm.controls.title.patchValue(post.title);
-          this.signupForm.controls.userId.patchValue(post.userId);
-        });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,26 @@ import {MyDirectiveDirective} from "./directives/my-directive.directive";
 import {MyPipePipe} from './pipes/my-pipe.pipe';
 import {ReactiveFormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { NewsComponent } from './components/news/news.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import {RouterModule, Routes} from "@angular/router";
+
+const appRoutes : Routes = [
+  { path : 'login',
+    component : LoginComponent} ,
+  { path : 'home',
+    component : HomeComponent ,
+    data : {page : 'Home page' }},
+  { path : 'news/:newsId' ,
+    component : NewsComponent },
+  { path : '',
+    redirectTo : '/home',
+    pathMatch : 'full'},
+  { path : '**',
+    component : NotFoundComponent}
+];
 
 @NgModule({
   declarations: [
@@ -17,12 +37,18 @@ import {HttpClientModule} from "@angular/common/http";
     TabConentComponent,
     TabComponentComponent,
     MyDirectiveDirective,
-    MyPipePipe
+    MyPipePipe,
+    HomeComponent,
+    LoginComponent,
+    NewsComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserModule ,
+    RouterModule. forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
