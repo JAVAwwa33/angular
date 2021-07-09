3c2259bd37d088004129d56b68918e9c95872381
lkre <lukasz.res@gmail.com>
2021-07-09 08:21:36 +0200

    Zad 23

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router, RouterEvent} from "@angular/router";
 
 @Component({
   selector: 'app-root',
@@ -6,10 +7,23 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+  isLoading = true;
 
-  constructor() {
+  constructor(private router: Router) {
+    router.events.subscribe((value) => this.checkEvent(value as RouterEvent));
   }
 
   ngOnInit(): void {
+  }
+
+  checkEvent(routeEvent: RouterEvent): void {
+    if (routeEvent instanceof NavigationStart) {
+      this.isLoading = true;
+    } else if (routeEvent instanceof NavigationEnd
+      || routeEvent instanceof NavigationCancel
+      || routeEvent instanceof NavigationError
+    ) {
+      this.isLoading = false;
+    }
   }
 }
